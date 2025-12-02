cddaba950c503b45d9f2ec14348e2cfdd6c77875
281015913 <281015913@qq.com>
2025-12-02 22:22:10 +0800

    Update LCMA.ino
    修改参数

--- LCMA.ino
+++ LCMA.ino
@@ -6,7 +6,7 @@
 
 #include "prime.h"  // 质数查找表12345672sss1
 #define BITS 1024
-#define NUM_STRINGS 10000  // 修改此处以生成多个字符串的数量ssss
+#define NUM_STRINGS 10000  // 修改此处以生成多个字符串的数量ssssa
 
 #define HASH_SIZE 32
 #define BLOCK_SIZE 64
